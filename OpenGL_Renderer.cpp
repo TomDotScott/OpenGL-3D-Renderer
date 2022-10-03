b68527dd1d896601be3a43f0cbf119b9db83e3d1
Tom Scott <tom.scott45456@gmail.com>
2022-10-03 16:13:49 +0100

    Create and Open GLFW Window

--- OpenGL_Renderer.cpp
+++ OpenGL_Renderer.cpp
@@ -1,20 +1,43 @@
-// OpenGL_Renderer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main()
 {
+    glfwInit();
+
+    // Tell glfw the version of OpenGL we're using
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    GLFWwindow* mainWindow = glfwCreateWindow(
+        800, 
+        600, 
+        "OpenGL Game", 
+        nullptr, 
+        nullptr
+    );
+
+    // If the window can't be created, let's kill the game
+    if(!mainWindow)
+    {
+	    std::cout << "Failed to create the GLFW Window!\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(mainWindow);
+
     std::cout << "Hello World!\n";
+
+    while(!glfwWindowShouldClose(mainWindow))
+    {
+        glfwPollEvents();
+    }
+
+    // Delete and stop GLFW when the program finishes
+    glfwDestroyWindow(mainWindow);
+    glfwTerminate();
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
