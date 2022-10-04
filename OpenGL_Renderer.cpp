c442f17588fd9c3dd5f3c577e6bf7a9f4a4a8280
Tom Scott <tom.scott45456@gmail.com>
2022-10-04 16:45:25 +0100

    3D Pyramid
    Using the perspective matrix, the model matrix when rotated gives perspective. Found the vertices for a Pyramid, and stole a texture from Mario to use for the pyramid

--- OpenGL_Renderer.cpp
+++ OpenGL_Renderer.cpp
@@ -15,23 +15,28 @@
 
 int main()
 {
-    // Vertex data for the quad
+    // Vertex data for the pyramid
     // XYZ go from -1 to 1
     // RGB is from  0 to 1
     // UV  is from  0 to n where n defines how much the texture will repeat
     constexpr GLfloat vertices[] =
     {
-    	// X |   Y  |  Z   |  Red  |  Green  |  Blue  |   U   |   V
-        -0.5f, -0.5f, 0.0f,   1.0f,   0.0f,     0.0f,   0.0f,    0.0f,  // Lower Left Corner
-        -0.5f,  0.5f, 0.0f,   0.0f,   1.0f,     0.0f,   0.0f,    1.0f,  // Lower Right Corner
-         0.5f,  0.5f, 0.0f,   0.0f,   0.0f,     1.0f,   1.0f,    1.0f,  // Upper Corner
-         0.5f, -0.5f, 0.0f,   1.0f,   1.0f,     1.0f,   1.0f,    0.0f   // Inner Left
+    	// X |   Y  |   Z   |  Red  |  Green  |  Blue  |  U   |    V
+        -0.5f,  0.0f,  0.5f,   0.8f,   0.72f,    0.44f,  0.0f,    0.0f,
+        -0.5f,  0.0f, -0.5f,   0.8f,   0.72f,    0.44f,  5.0f,    0.0f,
+         0.5f,  0.0f, -0.5f,   0.8f,   0.73f,    0.44f,  0.0f,    0.0f,
+         0.5f,  0.0f,  0.5f,   0.8f,   0.72f,    0.44f,  5.0f,    0.0f,
+         0.0f,  0.8f,  0.0f,   0.9f,   0.86f,    0.76f,  2.5f,    5.0f
     };
 
     constexpr GLuint indices[] =
     {
         0, 2, 1,
-        0, 3, 2
+        0, 2, 3,
+        0, 1, 4,
+        1, 2, 4,
+        2, 3, 4,
+        3, 0, 4
     };
 
 
@@ -109,21 +114,35 @@ int main()
     vbo1.Unbind();
     ebo1.Unbind();
 
-    Texture redPanda("Red_Panda.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-    redPanda.TexUniformUnit(shaderProgram, "tex0", 1);
+    Texture brickTexture("Mario_Bricks.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+    brickTexture.TexUniformUnit(shaderProgram, "tex0", 1);
 
     const GLuint tex0UniformID = glGetUniformLocation(shaderProgram.m_ID, "tex0");
     shaderProgram.Activate();
     glUniform1i(tex0UniformID, 0);
 
+
+    float pyramidRotation = 0.f;
+    double previousTime = glfwGetTime();
+
+    glEnable(GL_DEPTH_TEST);
+
     while(!glfwWindowShouldClose(mainWindow))
     {
         // Clear the screen before the render
         glClearColor(0.004f, 0.196f, 0.125f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Use the shader program
         shaderProgram.Activate();
+
+        const double currentTime = glfwGetTime();
+        // Every 60 frames-ish, rotate the pyramid
+        if(currentTime - previousTime >= static_cast<double>(1.f / 60.f))
+        {
+            pyramidRotation += 0.05f;
+            previousTime = currentTime;
+        }
 
         glm::mat4 modelMatrix = glm::mat4(1.f);
         glm::mat4 viewMatrix = glm::mat4(1.f);
@@ -140,23 +159,25 @@ int main()
             100.f
         );
 
+        // Rotate the model
+        modelMatrix = rotate(modelMatrix, pyramidRotation, glm::vec3(0.f, 1.f, 0.f));
+
         const GLint modelMatLocation = glGetUniformLocation(shaderProgram.m_ID, "modelMatrix");
         glUniformMatrix4fv(modelMatLocation, 1, GL_FALSE, value_ptr(modelMatrix));
 
         const GLint viewMatLocation = glGetUniformLocation(shaderProgram.m_ID, "viewMatrix");
         glUniformMatrix4fv(viewMatLocation, 1, GL_FALSE, value_ptr(viewMatrix));
 
-
         const GLint projectionMatLocation = glGetUniformLocation(shaderProgram.m_ID, "projectionMatrix");
         glUniformMatrix4fv(projectionMatLocation, 1, GL_FALSE, value_ptr(projectionMatrix));
 
         // To use our texture, we need to bind it!
-        glBindTexture(GL_TEXTURE_2D, redPanda.m_ID);
+        glBindTexture(GL_TEXTURE_2D, brickTexture.m_ID);
 
         vao1.Bind();
 
         // Draw the quad with the texture
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 
         // Swap the buffers to display the triangle on screen
         glfwSwapBuffers(mainWindow);
@@ -167,7 +188,7 @@ int main()
     vao1.Delete();
     vbo1.Delete();
     ebo1.Delete();
-    redPanda.Delete();
+    brickTexture.Delete();
     shaderProgram.Delete();
 
     // Delete and stop GLFW when the program finishes
